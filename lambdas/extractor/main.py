6f94dc56de44d187cd06f27014d1b04b356b700f
DaniV3611 <danivego2015@gmail.com>
2025-06-03 16:00:20 -0500

    Implementación de la funcionalidad de extracción de páginas: se añadieron módulos para realizar solicitudes HTTP, descargar y guardar páginas HTML, y subir archivos a S3. Se creó un manejador de errores para la descarga y se definieron funciones utilitarias para la gestión de directorios.

---
+++ lambdas/extractor/main.py
@@ -0,0 +1,21 @@
+from datetime import date
+
+from pages import download_and_save_pages, upload_pages_to_s3
+from utils import create_directory, delete_directory
+
+
+def main():
+
+    # Get the current date
+    curr_date = date.today().strftime("%Y-%m-%d")
+
+    dir_path = f"/tmp/periodico-{curr_date}"
+
+    create_directory(dir_path)
+
+    # Download and save the pages
+    pages_names = download_and_save_pages(dir_path, curr_date)
+
+    upload_pages_to_s3(pages_names)
+
+    delete_directory(dir_path)
---
+++ lambdas/extractor/proyecto.py
@@ -0,0 +1,23 @@
+import json
+from main import main
+
+
+def app(event, context):
+
+    try:
+        main()
+    except Exception as e:
+        print(f"❌ Error en la descarga de las páginas: {e}")
+        return {
+            "statusCode": 500,
+            "body": json.dumps({
+                "message": "❌ Error en la descarga de las páginas",
+            }),
+        }
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "✅ Se descargaron las páginas",
+        }),
+    }
